84fbf1835e260feea717326b1fc3b806977bf4a6
liuyong0922 <52902680+liuyong0922@users.noreply.github.com>
2023-02-15 13:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_weather():
   response = urllib.request.urlopen(request, context=ctx)
   content = response.read() 
   if (content):
-    print(content.encode('utf-8'))
+    print(content.text)
   weather = content['data']['forecast'][0]
   return weather['conditionDay'], math.floor(weather['tempNight'])
 
