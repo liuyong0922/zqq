d3d7dc37234a85f11d6f7737f3d94d4c698be287
liuyong0922 <52902680+liuyong0922@users.noreply.github.com>
2023-05-06 10:06:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -59,9 +59,9 @@ def get_birthday():
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
-#   if words.status_code != 200:
+  if words.status_code != 200:
     return get_words()
-  return words.json()['data']['text']
+  return words['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
